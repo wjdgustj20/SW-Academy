64d25d8b6f11cfe6558ef5345d8ecd6820646623
Hyunseo Jeong <90139789+wjdgustj20@users.noreply.github.com>
2023-01-12 13:12:47 +0900

    Delete programmers directory

--- programmers/BaseBall/src/main/java/engine/io/Input.java
+++
@@ -1,5 +0,0 @@
-package engine.io;
-
-public interface Input {
-    String input(String s);
-}
--- programmers/BaseBall/src/main/java/engine/io/NumberGenerator.java
+++
@@ -1,7 +0,0 @@
-package engine.io;
-
-import engine.model.Numbers;
-
-public interface NumberGenerator {
-    Numbers generate(int count);
-}
